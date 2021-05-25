9330cc6fc7be0f1ba00da5264524f8115a95f3d4
upgrade129 <premsantsh129@outlook.com>
2021-05-25 14:59:51 +0530

    change api

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/rates', (req, res) => {
-    fetch('https://api.openrates.io/latest')
+    fetch('https://free.currconv.com/api/v7/currencies?apiKey=4b72a471c99674009688')
     .then(res => res.json())
     .then(json => res.send(json))
 
@@ -20,14 +20,19 @@ app.get('/rates', (req, res) => {
 app.post('/base', (req, res) => {
     var fromcur = req.body.fromCurrency;
     var tocur = req.body.toCurrency;
-    console.log(fromcur,tocur);
-    fetch(`https://api.openrates.io/latest?base=${
-        fromcur}`)
-    .then(res => res.json())
-    .then(json => res.send(json))
-    
-    .catch((err)=> res.send(err));
- 
+    var fromId = req.body.fromId;
+    var toId = req.body.toId;
+
+    console.log(fromId,toId);
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      
+      fetch(`https://free.currconv.com/api/v7/convert?apiKey=4b72a471c99674009688&q=${fromId}_${toId}&compact=y`, requestOptions)
+        .then(response => response.text())
+        .then(result => res.send(result))
+        .catch(error => console.log('error', error));
 })
 
 
